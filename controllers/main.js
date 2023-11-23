cf3e551d5f8824ec7409648e246fc47fe719cca2
Stanislav Angelov <stannyweb@gmail.com>
2023-11-23 11:33:30 +0200

    Token verification

--- controllers/main.js
+++ controllers/main.js
@@ -16,11 +16,25 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 101)
-  res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your data, your lucky number is ${luckyNumber}`
-  })
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new CustomErrorAPI('No Token provided', 401)
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const luckyNumber = Math.floor(Math.random() * 101)
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your data, your lucky number is ${luckyNumber}`
+    })
+    console.log(decoded)
+  } catch (error) {
+    throw new CustomErrorAPI('Not Authorized to access this route!', 401)
+  }
 }
 
 module.exports = {
